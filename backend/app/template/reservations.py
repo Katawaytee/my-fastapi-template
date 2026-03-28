ea34849597d4c75eeca86a20622913f5d3e9e994
Katawaytee <ktriampo@gmail.com>
2026-03-28 16:29:15 +0700

    feat: implement reservation resource

---
+++ backend/app/template/reservations.py
@@ -0,0 +1,13 @@
+import logging
+from app.template.model.reservation import ReservationManager
+from fastapi import APIRouter
+
+router = APIRouter()
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class ReservationResource:
+    def __init__(self):
+        self.reservation_man = ReservationManager()
