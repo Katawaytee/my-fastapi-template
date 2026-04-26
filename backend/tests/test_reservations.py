989c55d863ac991609241ede42fa74adc60b67b1
Katawaytee <ktriampo@gmail.com>
2026-04-26 17:03:05 +0700

    test: add test case for reservation list

--- backend/tests/test_reservations.py
+++ backend/tests/test_reservations.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import pytest
 from app.core.response import ReturnStatus
 from app.template.model.reservation import (
@@ -20,7 +22,7 @@ from app.template.reservations import (
 MOCK_RESERVATION_IN = ReservationIn(
     customer_name="Customer One",
     party_size=4,
-    reservation_time="2026-11-17T16:59:00.000Z",
+    reservation_time=datetime(2026, 11, 17, 16, 59),
     table_id=3,
 )
 
@@ -69,3 +71,83 @@ async def test_reservation_bulk_add():
     assert res.status == ReturnStatus.SUCCESS.value
     assert res.info == "2 record(s) inserted"
 
+
+# ---------- LIST RESERVATION ---------- #
+@pytest.mark.asyncio
+async def test_reservation_list_default_params():
+    res = await reservation_list()
+
+    assert res.status == ReturnStatus.SUCCESS.value
+    assert res.count == 3
+    assert res.page == 0
+    assert res.page_size is None
+
+    data = res.data
+
+    assert data[0].reservation_id == created_reservation_id
+    assert data[0].customer_name == MOCK_RESERVATION_IN.customer_name
+    assert data[0].party_size == MOCK_RESERVATION_IN.party_size
+    assert data[0].reservation_time == MOCK_RESERVATION_IN.reservation_time
+    assert data[0].table_id == MOCK_RESERVATION_IN.table_id
+
+    # setup for followed test cases
+    global bulk_created_reservation_id
+    bulk_created_reservation_id = [
+        item.reservation_id
+        for item in data
+        if item.reservation_id != created_reservation_id
+    ]
+
+
+@pytest.mark.asyncio
+async def test_reservation_list_search_query():
+    res = await reservation_list(search_query="Two")
+
+    assert res.status == ReturnStatus.SUCCESS.value
+    assert res.count == 1
+    assert len(res.data) == 1
+
+    assert "Two" in res.data[0].customer_name
+
+
+@pytest.mark.asyncio
+async def test_reservation_list_order_by():
+    res = await reservation_list(order_by="reservation_id desc")
+
+    assert res.status == ReturnStatus.SUCCESS.value
+    assert res.count == 3
+    assert len(res.data) == 3
+
+    for i in range(1, 3):
+        assert res.data[i - 1].reservation_id > res.data[i].reservation_id
+
+
+@pytest.mark.asyncio
+async def test_reservation_list_pagination():
+    res = await reservation_list(page=0, page_size=2)
+
+    assert res.status == ReturnStatus.SUCCESS.value
+    assert res.count == 3
+    assert res.page == 0
+    assert res.page_size == 2
+
+    assert len(res.data) == 2
+
+
+@pytest.mark.asyncio
+async def test_reservation_list_all_params():
+    res = await reservation_list(
+        search_query="Customer T",
+        order_by="reservation_id desc",
+        page=0,
+        page_size=5,
+    )
+
+    assert res.status == ReturnStatus.SUCCESS.value
+    assert res.count == 2
+    assert res.page == 0
+    assert res.page_size == 5
+
+    assert len(res.data) == 2
+
+    assert res.data[0].reservation_id > res.data[1].reservation_id
